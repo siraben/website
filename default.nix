7449b8342159422c5e4e7ddc44367b0bac711b7c
Ben Siraphob <bensiraphob@gmail.com>
2022-01-02 16:15:42 +0700

    Make it a flake

--- default.nix
+++ default.nix
@@ -1,14 +1,25 @@
-{ srcs ? import ./nix/sources.nix, pkgs ? import srcs.nixpkgs {} }:
-with pkgs;
+(import (
+  let
+    lock = builtins.fromJSON (builtins.readFile ./flake.lock);
+  in fetchTarball {
+    url = "https://github.com/edolstra/flake-compat/archive/${lock.nodes.flake-compat.locked.rev}.tar.gz";
+    sha256 = lock.nodes.flake-compat.locked.narHash; }
+) {
+  src =  ./.;
+}).defaultNix
 
-let
-  jekyll_env = bundlerEnv rec {
-    name = "jekyll_env";
-    inherit ruby;
-    gemdir = ./.;
-  };
-in
-stdenv.mkDerivation rec {
-  name = "jekyll_env";
-  buildInputs = [ jekyll_env ];
-}
+  
+# { srcs ? import ./nix/sources.nix, pkgs ? import srcs.nixpkgs {} }:
+# with pkgs;
+
+# let
+#   jekyll_env = bundlerEnv rec {
+#     name = "jekyll_env";
+#     inherit ruby;
+#     gemdir = ./.;
+#   };
+# in
+# stdenv.mkDerivation rec {
+#   name = "jekyll_env";
+#   buildInputs = [ jekyll_env ];
+# }
---
+++ flake.nix
@@ -0,0 +1,41 @@
+{
+  description = "Sources for siraben.github.io";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    utils.url = "github:numtide/flake-utils";
+    flake-compat = {
+      url = "github:edolstra/flake-compat";
+      flake = false;
+    };
+  };
+
+  outputs = { self, nixpkgs, utils, ... }:
+    utils.lib.eachDefaultSystem (system:
+      with import nixpkgs { inherit system; };
+      let
+        jekyll_env = bundlerEnv rec {
+          name = "jekyll_env";
+          inherit ruby;
+          gemdir = ./.;
+        };
+      in
+        {
+          defaultPackage = stdenv.mkDerivation {
+            pname = "siraben.github.io";
+            version = "head";
+            src = ./.;
+            buildInputs = [ jekyll_env ];
+            buildPhase = ''
+              JEKYLL_ENV=production jekyll build --destination $out/public
+            '';
+            dontInstall = true;
+            # installPhase = "true";
+          };
+          devShell = stdenv.mkDerivation rec {
+            src = ./.;
+            name = "jekyll_env";
+            buildInputs = [ jekyll_env ];
+          };
+        }
+    );
+}
