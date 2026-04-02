209c7a7e69c5301b139d007b690d1a388e82eef4
Ben Siraphob <bensiraphob@gmail.com>
2026-04-02 16:39:23 -0700

    Modernize flake.nix
    - Remove flake-compat input
    - Use packages.default and devShells.default (non-deprecated)
    - Switch devShell from mkDerivation to mkShell
    - Use explicit pkgs binding instead of with import
    - Update nixpkgs to 2026-04-01

--- flake.nix
+++ flake.nix
@@ -1,40 +1,38 @@
 {
-  description = "Sources for siraben.github.io";
+  description = "Sources for siraben.dev";
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     utils.url = "github:numtide/flake-utils";
-    flake-compat = {
-      url = "github:edolstra/flake-compat";
-      flake = false;
-    };
   };
 
   outputs = { self, nixpkgs, utils, ... }:
     utils.lib.eachDefaultSystem (system:
-      with import nixpkgs { inherit system; };
       let
-        jekyll_env = bundlerEnv rec {
+        pkgs = import nixpkgs { inherit system; };
+        jekyll_env = pkgs.bundlerEnv {
           name = "jekyll_env";
-          inherit ruby;
+          ruby = pkgs.ruby;
           gemdir = ./.;
         };
       in
-        {
-          defaultPackage = stdenv.mkDerivation {
-            pname = "siraben.github.io";
-            version = "head";
-            src = ./.;
-            buildInputs = [ jekyll_env ];
-            buildPhase = ''
-              JEKYLL_ENV=production jekyll build --destination $out/public
-            '';
-            dontInstall = true;
-          };
-          devShell = stdenv.mkDerivation rec {
-            src = ./.;
-            name = "jekyll_env";
-            buildInputs = [ jekyll_env ];
-          };
-        }
+      {
+        packages.default = pkgs.stdenv.mkDerivation {
+          pname = "siraben.dev";
+          version = "head";
+          src = ./.;
+          buildInputs = [ jekyll_env ];
+          buildPhase = ''
+            JEKYLL_ENV=production jekyll build --destination $out/public
+          '';
+          dontInstall = true;
+        };
+
+        devShells.default = pkgs.mkShell {
+          buildInputs = [ jekyll_env ];
+          shellHook = ''
+            echo "Jekyll dev environment ready. Run 'jekyll serve' to start."
+          '';
+        };
+      }
     );
 }
